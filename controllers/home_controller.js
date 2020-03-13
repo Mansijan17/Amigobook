5dcfbec1c47bd5c277160f9c0439162b1ba29837
Manjari Jain <manjarijain98@gmail.com>
2020-03-13 07:23:11 +0530

    Displaying posts on the home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,35 @@
+const Post=require('../models/post');
+
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"});
+
+    // Post.find({},function(err,postLists)
+    // {
+        // if(err)
+        // {
+        //     console.log("Error in fetching data from db");
+        //     return;
+        // }
+        // // console.log(req.cookies);
+        // // res.cookie('user_id', 25);
+        // return res.render('home', {
+        // title: "Home",
+        // posts:postLists});
+    // })
+    
+
+    //populating the user of each post
+    Post.find({}).populate("user").exec(function(err,postLists)
+    {
+        if(err)
+        {
+            console.log("Error in fetching data from db");
+            return;
+        }
+        return res.render('home', {
+        title: "Home",
+        posts:postLists});
+    })
+    
 }
 
 
