4118f29eb2b7efa9b1bc7b7a5f4b5a5c445b0128
Manjari Jain <manjarijain98@gmail.com>
2020-03-13 08:35:47 +0530

    adding comment schema and refering to the post schema

---
+++ models/commentSchema.js
@@ -0,0 +1,28 @@
+const mongoose=require('mongoose');
+
+const commentSchema=new mongoose.Schema({
+    content:
+    {
+        type:String,
+        required:true
+    },
+    //comments belongs to the post
+    post:
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Post"
+
+    },
+     //comments belongs to the user
+    user:
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+
+},{
+    timestamps:true
+});
+
+const Comment=mongoose.model("Comment",commentSchema);
+module.exports=Comment;
