1deac8906a8779275ddf7e9c1b255c4d9616db98
Manjari Jain <manjarijain98@gmail.com>
2020-03-13 08:11:44 +0530

    passport authentication for posts

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const password=require('passport');
 
 const postController=require('../controllers/post_controller');
 
-router.post("/create-post",postController.createPost);
+router.post("/create-post",password.checkAuthentication,postController.createPost);
 
 module.exports=router;
