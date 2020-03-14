b273dfa4f406111c2440980624224aca4b61667b
Manjari Jain <manjarijain98@gmail.com>
2020-03-14 19:45:05 +0530

    delete post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,5 +5,5 @@ const password=require('passport');
 const postController=require('../controllers/post_controller');
 
 router.post("/create-post",password.checkAuthentication,postController.createPost);
-
+router.get("/destroy-post/:id",password.checkAuthentication,postController.destroyPost);
 module.exports=router;
