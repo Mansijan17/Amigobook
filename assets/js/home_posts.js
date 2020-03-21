cec90666b6092c3d2f981b4c65e8372009092c9a
Manjari Jain <manjarijain98@gmail.com>
2020-03-21 09:45:43 +0530

    recieving data using ajax of post

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,12 +10,13 @@
             e.preventDefault();
 
             $.ajax({
-                method:"post",
+                type:"post",
                 url:"/posts/create-post",
                 data:newPostForm.serialize(),
                 success:function(data)
                 {
-                    console.log(data);
+                    let newPost=newDomPost(data.data.post);
+                    $("#posts-list-container>ul").prepend(newPost);
                 },
                 error:function(err)
                 {
@@ -24,7 +25,37 @@
             })
         })
     }
-    creatPost();
+    
 
     //method to create post in DOM
+    let newDomPost=function(i)
+    {
+        return $(`
+
+        <li style="text-align: left;" id="post-${ i._id}">
+           
+                <small>
+                    <a href="/posts/destroy-post/${ i.id }" class="delete-post-button">Delete Post</a>
+                </small>
+         
+            <p>
+                ${ i.content}
+                <small>${ i.user.name }</small>
+            </p>
+            <div class="post-comments">
+                <form action="/comments/create-comment" method="post">
+                    <input type="text" name="content" placeholder="Type here to add comment...." required>
+                    <input type="hidden" name="post" value="${ i._id }">
+                    <button type="submit">Add comment</button>
+                </form>
+            </div>
+            <div class="post-comments-list">
+                <ul id="post-comments-${i._id}">
+                   
+                </ul>
+        
+            </div>
+        </li>`);
+    }
+    creatPost();
 }
