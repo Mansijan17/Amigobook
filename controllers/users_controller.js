684f8960862bad51f2babcc2e0d5297d2bbc0863
Manjari Jain <manjarijain98@gmail.com>
2020-03-24 19:29:12 +0530

    saving file in the uploads folder

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,18 +13,55 @@ module.exports.profile = async function(req, res){
    
 }
 
-module.exports.update=function(req,res)
+module.exports.update= async function(req,res)
 {
+    // if(req.user.id==req.params.id)
+    // {
+    //     User.findByIdAndUpdate(req.params.id,req.body,function(err,user)
+    //     {
+    //         req.flash("success","Updated!");
+    //         return res.redirect("back");
+    //     })
+        
+    // }
+    // else
+    // {
+    //    req.flash("error","Unauthorized!");
+    //     return res.status(401).send("Unauthorised");
+    // }
+
     if(req.user.id==req.params.id)
     {
-        User.findByIdAndUpdate(req.params.id,req.body,function(err,user)
+        try
         {
+            let user=await User.findById(req.params.id);
+            User.uploadedAvatar(req,res,function(err)
+            {
+                if(err)
+                {
+                    console.log("****Multer ",err);
+                }
+                //console.log(req.file);
+                user.name=req.body.name;
+                user.email=req.body.email;
+                if(req.file)
+                {
+                    //this is saving the path of the file in the avatar field of the user
+                    user.avatar=User.avatarPath+"/"+req.file.filename;
+                }
+                user.save();
+                return res.redirect("back");
+            })
+        }
+        catch(err)
+        {
+            req.flash("error",err);
             return res.redirect("back");
-        })
-        
+        }
     }
     else
     {
+        req.flash("error","Unauthorized!");
         return res.status(401).send("Unauthorised");
     }
 }
