6391e591c9bd1abba8f301f0742e427b0c8092c3
Manjari Jain <manjarijain98@gmail.com>
2020-03-25 10:16:24 +0530

    displaying profile image to all the users

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -50,6 +50,7 @@ module.exports.update= async function(req,res)
                     user.avatar=User.avatarPath+"/"+req.file.filename;
                 }
                 user.save();
+                req.flash("success","Updated!");
                 return res.redirect("back");
             })
         }
