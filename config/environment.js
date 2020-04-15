696d067110c526d7c1cbd7c12eb9b623684aff9a
Manjari Jain <manjarijain98@gmail.com>
2020-04-15 11:10:49 +0530

    updating files

--- config/environment.js
+++ config/environment.js
@@ -24,12 +24,12 @@ const development={
         port:587,
         secure:false,
         auth:{
-            user:'manjarijain1998@gmail.com',
-            pass:'101998Mj'
+            user:'email@gmail.com',
+            pass:'password'
         }
     },
-    google_client_id:"277058453568-l24k6eqhmfi696gn75g71oskubkm0l18.apps.googleusercontent.com",
-    google_client_secret:"vZaDWh1SxGf0PHetHISeZu_X",
+    google_client_id:"***********************",
+    google_client_secret:"********************",
     google_call_back_url:"http://localhost:8000/users/auth/google/callback",
     jwt_secret:"codeial",
     morgan:{
