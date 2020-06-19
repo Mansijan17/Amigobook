db20793cbc143a3948980b3e3b9eb4e40a3497f3
manjari jain <manjarijain98@gmail.com>
2020-06-19 05:34:49 +0530

    changes

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ chatServer.listen(5000);
 console.log("chat server is listening on port 5000");
 
 const path=require('path');
-// app.use(SassMiddleware({
-//         src:path.join(__dirname,env.asset_path,"/scss"),
-//         dest:path.join(__dirname,env.asset_path,"/css"),
-//         debug:true,
-//         outputStyle:"extended",
-//         prefix:"/css"
-//  }));    
+app.use(SassMiddleware({
+        src:path.join(__dirname,env.asset_path,"/scss"),
+        dest:path.join(__dirname,env.asset_path,"/css"),
+        debug:true,
+        outputStyle:"extended",
+        prefix:"/css"
+ }));    
 
 
 app.use(express.urlencoded());
